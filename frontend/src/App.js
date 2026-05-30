6d3fbc08c490efd52230fb7ff1c206487e1e995b
Atluri Satheesh <atlurisatheesh93@gmail.com>
2026-05-30 18:35:42 +0530

    Add LandingPage component to frontend app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,15 @@ import '@/App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainApp from '@/components/MainApp';
 
+import LandingPage from '@/components/LandingPage';
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<MainApp />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/app" element={<MainApp />} />
         </Routes>
       </BrowserRouter>
     </div>
